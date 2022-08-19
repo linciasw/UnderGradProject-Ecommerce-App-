417d8347e9bafcc965eecf6392922fd99aa578c8
Lincia Walters <lincia.walters@hotmail.com>
2022-08-19 09:05:04 -0400

    Cart functionality and design completed

--- app/Controllers/Cart.php
+++ app/Controllers/Cart.php
@@ -4,6 +4,7 @@
 require_once APP_DIR . "Config/Database.php";
 require_once APP_DIR . "Models/User.php";
 require_once APP_DIR . "Models/Cart.php";
+require_once APP_DIR . "Utils/code.isLoggedIn.php";
 
 //create objects
 $db_object = new Database();
@@ -19,11 +20,19 @@ $cart_object = new Cart($db_object);
 //$cart_object->addToCart(2, 2, 2);
 //$cart_object->removeFromCart(2, 1);
 
-$cart_details = $cart_object->getCartDetails(1);
-debug($cart_details);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if (isset($_POST["cart_id"])) {
+        $cart_object->removeFromCart($_POST["cart_id"], $user_id);
+    }
+}
+
+
+$cart_details = $cart_object->getCartDetails($user_id);
+//debug($cart_details);
 
 
 //load views
-require_once APP_DIR . "Views/header.php";
+require_once APP_DIR . "Views/header1.php";
 require_once APP_DIR . "Views/pages/cart.php";
 require_once APP_DIR . "Views/footer.php";
