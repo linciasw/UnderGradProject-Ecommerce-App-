7b1e0101219db09bde651a3a34ac56f5a7446370
Lincia Walters <lincia.walters@hotmail.com>
2022-09-02 19:33:05 -0400

    Shopping Tools pages updated

--- app/Views/pages/hard-drive.php
+++ app/Views/pages/hard-drive.php
@@ -16,38 +16,52 @@
 
     <div class="row">
         <div class="col-lg-10 mb-4">
-        <h3>Hard Drive Buying Guide</h3>
-            <p>So, you’re in the market for a new computer but aren’t exactly sure what device you want or need? </p>
+        <h3>How To Choose The Best Hard Drive for Desktop PCs?</h3>
+            <p>Whether you’re building a PC for the first time or just looking to upgrade the storage components in your desktop PC, picking the right storage option is crucial for getting optimal performance in your system. Often overlooked as something that you need to buy “bigger” to be better, hard drives and storage devices have evolved a bit from their heavy-set historical past. At first, it may seem daunting to keep up with the different formats and technologies. What follows in this buying guide will help you choose the best storage for your desktop PC.</p>
             <p>
-            What follows in this guide will help you have a strong understanding of what pre-built desktop PCs can offer you, as well as the multiple variations of these products. 
-            <br>
-            If you can live without the portability of a laptop or tablet, desktop PCs can provide you with more power as well as a different form of convenience. Compared to laptops, most pre-built PCs are more upgradable. Sure you can swap out hard drives, SSDs, and memory sticks on some laptops, but their CPUs and graphics are often soldered in, so your processing and graphics power will stay the same until your next purchase.
-            Depending on the manufacturer, pre-built PCs can swap out most, if not all, components, including the graphics card, motherboard, and CPU. You do need to make sure to check back on your specifications though, whenever you’re changing out components.
-            <br>
-            Another great thing about desktops? You don’t need to worry about charging a battery. Just plug it into a viable outlet and you’re good to go. Fortunately, since you’re going with a pre-built instead of do-it-yourself, you also don’t initially have to worry about compatibility issues or making sure your PSU can handle your components. 
+            What is internal desktop PC storage? As the name implies, internal desktop PC storage consists of components found within the chassis of any given system. This is true whether the device is a tower desktop or laptop. PC storage components install by a physical connection to either your computer motherboard’s SATA or PCI-E ports. By comparison, external storage consists of storage devices that come equipped with their own chassis. 
             </p>
-            <br>
 
-            <h2>Operating Systems</h2>
+
+            <br>
+            <br>
+            <p>
+            Internal storage types
+            </p>
+
 
             <p>
-            Without an operating system, desktop PCs would just be immense paperweights with limited computing functions. A computer’s operating system is the overarching software that connects physical hardware and software applications together.
-            Windows- Made by Microsoft, Windows is one of the world’s most popular operating systems with the most current version being Windows 10. If you’re a hardcore gamer, Windows should be your operating system of choice due to the fact that most of the latest PC games are only available on Windows. Like the majority of their releases, Microsoft’s Windows 10 comes in a Pro and Home edition. The main difference between these versions is that Pro comes with enterprise business features such as group policy management, remote desktop and other IT-related business support. It’s best to use the latest version of the OS but, but as of writing, the Windows 7 operating system is the minimum requirement for games like Sekiro: Shadows Die Twice or video-editing applications like Adobe Premiere.
-            Mac- The other major entry at the forefront of popular operating systems for desktop PCs is Apple’s macOS. As of writing the most current version of Apple’s flagship operating system is macOS Mojave. The Mac platform houses a fair amount of support for the latest games, but not enough to be useful to hardcore PC gamers. Whereas Windows can be considered an open environment when it comes to modding or developing components and software, Apple’s system is closed. You have to abide by Apple’s rules, but the protection and privacy options you get with your Mac is top-notch. But this also means upgrades or self-repairs cannot be as easily executed on your Mac system. Nevertheless, millions of users love the features of macOS and Apple’s computers have become known as the machines of choice for artists and designers. Though prices may be high, the power and lifespan of the latest products generally match the initial investment.
-            Chrome OS- A fairly new competitor, Google’s Chrome OS is an operating system based off Linux. Similar to their Android-based mobile operating systems, the latest Chrome OS desktop PCs utilize the Google Play Store for getting apps like Microsoft Office programs or streaming services like Netflix or YouTube.  Chrome OS desktop PCs can be highly cost-effective and useful for simple home entertainment, student work or small- to mid-sized businesses.
-            Linux- This free, open-source operating system is highly efficient and effective for those interested in running server or large mainframe systems. Currently, one of the most-used distributions (distros) of Linux is called Ubuntu, but the beautiful thing about Linux is that users can choose from a number of publicly shared distributions and decide whichever one best fits their purposes.
+            Like all things in computing, storage hardware comes in many different shapes and sizes. The types of storage drive we’re talking about today are specifically for common daily-computing as opposed to NAS (Network-Attached-Storage) or enterprise-level scenarios. At the end of the day, you are just storing stuff—but knowing each device’s intricacies and their technological history will give you a better understanding of how to implement them.
+            </p>
+
+            <p>
+            Hard Disk Drives (HDDs)
+            </p>
+
+            <p>
+            The oldest device that we’ll be covering today, hard disk drives, more commonly referred to as hard drives, are mechanical components that use electromagnetism to store data. First introduced in the 1950s with IBM’s 305 RAMAC computer, the first magnetic hard disks changed the world by allowing users to finally access their saved information instantly. Unlike most things in the field of technology, hard drives have withstood the test of time as they are still relevant in today’s systems. But, exactly how does a hard drive work?
+            <br>
+            When you open a hard drive you’ll see a setup that looks very similar to a record player’s needle on a record. The basic difference comes in the form of an HDD’s applied magnetism. Without touching, the hard drive’s magnetic head (record player’s needle) on its actuator arm is able to access any part of the spinning platter that has also been coated with magnetic material.
+            Despite being relatively old technology, hard drives have continued to be a popular storage option. This is thanks to years of solid performance and expanding storage space. But, HDDs do have their faults and limitations, and aren’t as fast or feature-rich as newer alternatives.
+
+
             </p>
 
 
 
+            <p>
+            Solid State Drives (SSDs)
+            </p>
 
+            <p>
+            Due to their mechanical components, hard drives are a bit more prone to physical defects. They’re also not great at withstanding shock damage from being dropped or moved sharply. Because a hard drive has to spin extremely fast (up to 7,200 rotations per minute [RPM] for desktop-level products) there is a finite physical limitation to how fast your data can be accessed or moved.
+            <br>
+            SSDs are powered and effectively function by way of electronic circuits—and zero moving parts. If you remove the chassis of an SSD you’ll see something very similar to an electronic circuit board. Along with some of the main components found on most circuit boards, SSDs come equipped with flash memory chips (the SSD’s version of an HDD’s spinning disk) and a flash controller (the SSD’s take on the HDD’s magnetic actuator arm). With this format, you’re no longer limited to the physics of spinning and magnetism, but rather the quality and engineered-design of each circuit board. Simply put, this allows for better speed and reliability.
+            Like hard drives, SSDs have evolved in many ways since their inception, including form factor. The base consumer-level SSDs for desktop PCS come in a hard-drive-like chassis—just much lighter and smaller, with the most common size today being 2.5”. Subsequently, the mSATA and M.2 form factors improve performance in an even smaller and lighter package.
 
 
             </p>
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
-        </div>
-        <div class="col-lg-8">
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur <strong class="font-weight-bold text-dark">adipisicing elit, sed </strong>do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <strong class="font-weight-bold text-dark">reprehenderit in voluptate </strong>velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
         </div>
     </div>
 </div>
--- app/Views/pages/laptop.php
+++ app/Views/pages/laptop.php
@@ -17,37 +17,42 @@
     <div class="row">
         <div class="col-lg-10 mb-4">
         <h3>Laptop Buying Guide</h3>
-            <p>So, you’re in the market for a new computer but aren’t exactly sure what device you want or need? </p>
+            <p>How To Choose A Laptop </p>
             <p>
-            What follows in this guide will help you have a strong understanding of what pre-built desktop PCs can offer you, as well as the multiple variations of these products. 
+            There’s never been a better time to buy a notebook PC. Today’s options are more powerful, more solidly built, longer lasting, and better looking than ever. Whether you want to run Windows 10, macOS, or Chrome OS, there’s likely a notebook that will meet your needs at a price you can afford.
             <br>
-            If you can live without the portability of a laptop or tablet, desktop PCs can provide you with more power as well as a different form of convenience. Compared to laptops, most pre-built PCs are more upgradable. Sure you can swap out hard drives, SSDs, and memory sticks on some laptops, but their CPUs and graphics are often soldered in, so your processing and graphics power will stay the same until your next purchase.
-            Depending on the manufacturer, pre-built PCs can swap out most, if not all, components, including the graphics card, motherboard, and CPU. You do need to make sure to check back on your specifications though, whenever you’re changing out components.
+            Of course all of that variety makes choosing a notebook harder than ever. There are so many different makes and models to choose from that you need to do your research to make sure you’re getting just the right notebook for you.
             <br>
-            Another great thing about desktops? You don’t need to worry about charging a battery. Just plug it into a viable outlet and you’re good to go. Fortunately, since you’re going with a pre-built instead of do-it-yourself, you also don’t initially have to worry about compatibility issues or making sure your PSU can handle your components. 
+            That’s why we put this guide together, to provide an overview of the things you’ll want to consider before pulling out your credit card.
             </p>
             <br>
 
-            <h2>Operating Systems</h2>
+            <p>Form Factor</p>
 
             <p>
-            Without an operating system, desktop PCs would just be immense paperweights with limited computing functions. A computer’s operating system is the overarching software that connects physical hardware and software applications together.
-            Windows- Made by Microsoft, Windows is one of the world’s most popular operating systems with the most current version being Windows 10. If you’re a hardcore gamer, Windows should be your operating system of choice due to the fact that most of the latest PC games are only available on Windows. Like the majority of their releases, Microsoft’s Windows 10 comes in a Pro and Home edition. The main difference between these versions is that Pro comes with enterprise business features such as group policy management, remote desktop and other IT-related business support. It’s best to use the latest version of the OS but, but as of writing, the Windows 7 operating system is the minimum requirement for games like Sekiro: Shadows Die Twice or video-editing applications like Adobe Premiere.
-            Mac- The other major entry at the forefront of popular operating systems for desktop PCs is Apple’s macOS. As of writing the most current version of Apple’s flagship operating system is macOS Mojave. The Mac platform houses a fair amount of support for the latest games, but not enough to be useful to hardcore PC gamers. Whereas Windows can be considered an open environment when it comes to modding or developing components and software, Apple’s system is closed. You have to abide by Apple’s rules, but the protection and privacy options you get with your Mac is top-notch. But this also means upgrades or self-repairs cannot be as easily executed on your Mac system. Nevertheless, millions of users love the features of macOS and Apple’s computers have become known as the machines of choice for artists and designers. Though prices may be high, the power and lifespan of the latest products generally match the initial investment.
-            Chrome OS- A fairly new competitor, Google’s Chrome OS is an operating system based off Linux. Similar to their Android-based mobile operating systems, the latest Chrome OS desktop PCs utilize the Google Play Store for getting apps like Microsoft Office programs or streaming services like Netflix or YouTube.  Chrome OS desktop PCs can be highly cost-effective and useful for simple home entertainment, student work or small- to mid-sized businesses.
-            Linux- This free, open-source operating system is highly efficient and effective for those interested in running server or large mainframe systems. Currently, one of the most-used distributions (distros) of Linux is called Ubuntu, but the beautiful thing about Linux is that users can choose from a number of publicly shared distributions and decide whichever one best fits their purposes.
+            Once upon a time, virtually all notebooks were of the clamshell variety, meaning you open the lid to reveal the display and uncover the keyboard located in the other half. That’s still a popular form factor, but it’s no longer the only option, and choosing between the alternatives is perhaps the biggest choice you’ll make.
             </p>
 
+            <p>
+            Clamshells - The clamshell notebook remains a popular choice, and for many brands it’s the only option available. Just about every notebook manufacturer produces clamshell versions, which tend to build in the most powerful components and come in the largest sizes, up to 17 inches and even larger.
+            You’ll find clamshell notebooks running every available operating system today, including Windows 10, macOS, Chrome OS, and even Linux.
+            </p>
 
+            <p>
+            Convertible 2-in-1s - An increasingly popular form factor is the convertible 2-in-1, which usually means a notebook that starts out like a clamshell but with a display that can be twisted all the way around until the notebook turns into a tablet. Thanks to their 360-degree hinges, convertible 2-in-1s provide considerable flexibility, typically offering four different usage options: clamshell, tent mode (an inverted “v”), presentation mode (where the keyboard is facing down, and the display faces the user), and tablet mode.
+            Convertible 2-in-1 displays usually range in size from 11 inches to 15 inches, and they tend to be relatively thin and light. After all, nobody wants to hold a tablet that’s too thick or heavy. Some convertible 2-in-1s are equipped with fast central processing units (CPUs) and discrete graphics processing units (GPUs), making them perform well in light gaming and content creation tasks.
+            Also, many convertible 2-in-1s support active pens, and thus they work well with Windows 10 Ink that allows for writing and drawing on the display. Apple has yet to introduce a macOS 2-in-1 device, and so most run Windows 10. More recently, though, Chrome OS is becoming a popular 2-in-1 option as well.
+            </p>
 
-
+            <p>
+            Detachable Tablet - Apple’s iPad kicked off the modern tablet market, but it didn’t take long for Windows manufacturers to join in. Microsoft started with its original Surface device, and today many companies are making Windows 10 tablets that are light and relatively powerful.
+            Usually, tablets support detachable keyboards, which means that users can deploy them as traditional notebooks. Tablets are typically smaller than the other form factors, with most using displays between 10 and 13.3 inches. Because they’re so thin and light, they tend to use lower-powered CPUs and integrated graphics, but some offer entry-level discrete GPUs as well.
+            As with convertible 2-in-1s, Apple doesn’t make a macOS tablet, and so your choices of operating system will be limited to Windows 10 and, recently, Chrome OS.
 
 
             </p>
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor.</p>
+
         </div>
-        <div class="col-lg-8">
-            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur <strong class="font-weight-bold text-dark">adipisicing elit, sed </strong>do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in <strong class="font-weight-bold text-dark">reprehenderit in voluptate </strong>velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-        </div>
+
     </div>
 </div>
