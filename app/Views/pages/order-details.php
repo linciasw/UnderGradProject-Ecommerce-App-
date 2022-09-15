1346363f2699579457f92262c1a26ed7939dc793
Lincia Walters <lincia.walters@hotmail.com>
2022-09-15 14:54:40 -0400

    Oder details page edited

--- app/Views/pages/order-details.php
+++ app/Views/pages/order-details.php
@@ -28,7 +28,7 @@
     <div class="row text-center text-black mb-5">
         <div class="col-lg-7 mx-auto">
             <h1 class="display-4">Order Details</h1>
-            <a href="<?php echo BASE_URL . "orders" ?>">Return To Product Listing</a>
+            <a href="<?php echo BASE_URL . "orders" ?>">Return To Order Listing</a>
         </div>
     </div>
     <div class="row">
@@ -45,10 +45,10 @@
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
                         <div class="media-body order-2 order-lg-1">
                             <h5><?php echo $data["product_title"]; ?></h5>
-                            <h4>Date ordered: <?php echo $data["order_details_created"]; ?></h4>
+                            <h4>Date & time ordered: <?php echo $data["order_details_created"]; ?></h4>
                             <!-- <p class="font-italic text-muted mb-0 small">128 GB ROM | 15.49 cm (6.1 inch) Display 12MP Rear Camera | 7MP Front Camera A12 Bionic Chip Processor</p> -->
                             <div class="d-flex align-items-center justify-content-between mt-1">
-                                <h6 class="font-weight-bold my-2"><?php echo $data["order_details_price"]; ?></h6>
+                                <!-- <h6 class="font-weight-bold my-2"><?php echo $data["order_details_price"]; ?></h6> -->
                                 <ul class="list-inline small">
                                     <li class="list-inline-item m-0"><i class="fa fa-star-o text-gray"></i></li>
                                 </ul>
@@ -64,7 +64,7 @@
                     <!-- Custom content-->
                     <div class="media align-items-lg-center flex-column flex-lg-row p-3">
                         <div class="media-body order-2 order-lg-1">
-                        <h1 class="text-center">Summary</h1>
+                        <h1 class="text-center">Order Summary</h1>
                         <h3>Order # <span class="float-right"><?php echo $data["order_id"]; ?></span></h3>
                         <h3>Subtotal <span class="float-right"><?php echo $data["subtotal"]; ?></span></h3>
                         <h3>Total <span class="float-right"><?php echo $data["total"]; ?></span></h3>
